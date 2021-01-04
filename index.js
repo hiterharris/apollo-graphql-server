e8ba711ab1e976caf174fd90057428e0925a1b45
hiterharris <hiterharris@comcast.net>
2021-01-04 09:02:23 -0500

    add mutations

--- index.js
+++ index.js
@@ -13,25 +13,20 @@ const books = [
     },
 ];
 
-const authors = [
-    {
-        id: 1,
-        name: 'Kate Chopin',
-    },
-    {
-        id: 2,
-        name: 'Paul Auster',
-    }
-];
-
 const users = [
     {
         id: 1,
         name: 'Hiter',
+        email: 'hiter@gmail.com',
+        username: 'hiter',
+        password: 'pass',
     },
     {
         id: 2,
         name: 'Noor',
+        email: 'noor@gmail.com',
+        username: 'noor',
+        password: 'pass02',
     }
 ];
 
@@ -43,21 +38,23 @@ const typeDefs = gql`
     author: String
   }
 
-  type Author {
-      id: ID!
-      name: String
-  }
-
   type User {
     id: ID!
     name: String
+    email: String,
+    username: String
+    password: String
   }
   
   type Query {
     books: [Book]
-    authors: [Author]
     users: [User]
     user(id: ID!): User
+  }
+
+  type Mutation {
+    addBook(title: String, author: String): Book
+    addUser(name: String, email: String, username: String, password: String): User
   }
   
 `;
@@ -65,12 +62,33 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
       books: () => books,
-      authors: () => authors,
       users: () => users,
-      user(parent, args, context, info) {
+      user(parent, args) {
           return users.find(user => user.id == args.id);
       }
     },
+    Mutation: {
+      addBook: (parent, args) => {
+        const newBook = {
+          id: books.length + 1,
+          title: args.title,
+          author: args.author
+        };
+        books.push(newBook);
+        return newBook;
+      },
+      addUser: (parent, args) => {
+        const newUser = {
+          id: users.length + 1,
+          name: args.name,
+          email: args.email,
+          username: args.username,
+          password: args.password
+        };
+        users.push(newUser);
+        return newUser;
+      }
+    }
   };
   
 const server = new ApolloServer({ typeDefs, resolvers });
